893ec66e7b658a1fffd1992715632826a3c9decd
quang.pt <quang.pt@aris-vn.com>
2020-02-28 13:59:12 +0700

    update source code
    update source code

---
+++ Application/ViewController/src/web/view/backing/Template.java
@@ -0,0 +1,82 @@
+package web.view.backing;
+
+import oracle.adf.view.rich.component.rich.RichDocument;
+import oracle.adf.view.rich.component.rich.RichForm;
+import oracle.adf.view.rich.component.rich.layout.RichGridCell;
+import oracle.adf.view.rich.component.rich.layout.RichGridRow;
+import oracle.adf.view.rich.component.rich.layout.RichPanelGridLayout;
+
+public class Template {
+    private RichGridCell gc2;
+    private RichGridCell gc3;
+    private RichGridRow gr2;
+    private RichGridCell gc1;
+    private RichGridRow gr1;
+    private RichPanelGridLayout pgl1;
+    private RichForm f1;
+    private RichDocument d1;
+
+    public void setGc2(RichGridCell gc2) {
+        this.gc2 = gc2;
+    }
+
+    public RichGridCell getGc2() {
+        return gc2;
+    }
+
+    public void setGc3(RichGridCell gc3) {
+        this.gc3 = gc3;
+    }
+
+    public RichGridCell getGc3() {
+        return gc3;
+    }
+
+    public void setGr2(RichGridRow gr2) {
+        this.gr2 = gr2;
+    }
+
+    public RichGridRow getGr2() {
+        return gr2;
+    }
+
+    public void setGc1(RichGridCell gc1) {
+        this.gc1 = gc1;
+    }
+
+    public RichGridCell getGc1() {
+        return gc1;
+    }
+
+    public void setGr1(RichGridRow gr1) {
+        this.gr1 = gr1;
+    }
+
+    public RichGridRow getGr1() {
+        return gr1;
+    }
+
+    public void setPgl1(RichPanelGridLayout pgl1) {
+        this.pgl1 = pgl1;
+    }
+
+    public RichPanelGridLayout getPgl1() {
+        return pgl1;
+    }
+
+    public void setF1(RichForm f1) {
+        this.f1 = f1;
+    }
+
+    public RichForm getF1() {
+        return f1;
+    }
+
+    public void setD1(RichDocument d1) {
+        this.d1 = d1;
+    }
+
+    public RichDocument getD1() {
+        return d1;
+    }
+}
